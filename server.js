053dc30fc0478748d8281dfc1ff6f18f27618a5c
varun <varunshinde04vrn@gmail.com>
2025-09-10 21:37:38 +0530

    updated server.js

--- server.js
+++ server.js
@@ -8,16 +8,20 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/api/response', async(req, res) => {
-    const data = req.body
-    let response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.Gemini_API_Key}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    let result = await response.json();
-    res.json(result)
+    try {
+        const data = req.body
+        let response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.Gemini_API_Key}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        let result = await response.json();
+        res.json(result);
+    } catch(err) {
+        res.status(505).json({error: err});
+    }
 })
 
 app.listen(port, () => {
